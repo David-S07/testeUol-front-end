2758b776ef2d58529ad59289e8eea59f44db96f2
David-S07 <david.tata@hotmail.com>
2022-05-10 02:48:40 -0300

    front-end teste UOL

---
+++ README.md
@@ -0,0 +1,9 @@
+# APP
+
+Projeto criado em Angular versão 12.2.6.
+
+## Pra Subir a APP
+
+digitar no terminal `ng run start`.
+
+Acessar `http://localhost:4200/`.
---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { Incidente } from './incidente/incidente';
+import { IncidenteService } from './incidente/incidente.service';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  incidentes : Incidente[] = [];
+
+  constructor(private incidenteService: IncidenteService) {
+
+      incidenteService.getIncidentes()
+      .subscribe(incidente => this.incidentes = incidente);
+    }
+
+}
